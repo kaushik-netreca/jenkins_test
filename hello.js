0eb72603dcc2f18b2ee170898b179dfd9c70d213
Kaushik Ponugoti <kaushik@netreca.com>
2018-05-16 15:13:49 -0700

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+// Just a sample hello world daemon program
+
+var express = require('express');
+var app = express();
+
+app.get('/test', function(req,res){
+  res.send('Hello World!!');
+});
+
+app.listen(3000,function(){
+  console.log('Test');
+});
