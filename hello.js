e2d0e1fa5451061a7c42b1602e7305deef53d2af
Kaushik Ponugoti <kaushik@netreca.com>
2018-05-16 15:56:10 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/test', function(req,res){
-  res.send('Hello Kaushik!!');
+  res.send('Hello POSMaster!! How Are You ??');
 });
 
 app.listen(3000,function(){
