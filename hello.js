731c011e60928dd8c06372f09593eb5e3b404d12
Kaushik Ponugoti <kaushik@netreca.com>
2018-06-05 09:08:38 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/test', function(req,res){
-  res.send('Hello Netreca..!');
+  res.send('Hello World..!');
 });
 
 app.listen(3000,function(){
